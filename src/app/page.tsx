137f70195b3d0737c9bd92eabe42641ef06ca62d
Shubhamnile22 <theshubhamnile@gmail.com>
2025-12-11 19:42:49 +0530

    fix: merge changes and added footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Hero } from "@/components";
 import { ApplySteps } from "@/components/ApplySteps";
 import OffersSection from "@/components/Offers/OffersSection";
 import WhyUs from "@/components/WhyUs/WhyUs";
+import Footer from "@/components/Footer/Footer";
 import React from "react";
 
 export default function Home() {
@@ -11,6 +12,8 @@ export default function Home() {
       <OffersSection />
       <ApplySteps />
       <WhyUs />
+
+    <Footer />
     </div>
   );
 }
