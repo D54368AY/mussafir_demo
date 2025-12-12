79e3750354f58e4da40df6e6502b599ee203f7dc
rushikesh <rushiahire9567@gmail.com>
2025-12-12 11:15:03 +0530

    fix: offer section UI

--- src/components/Offers/BannerCarousel/BannerCarousel.tsx
+++ src/components/Offers/BannerCarousel/BannerCarousel.tsx
@@ -10,20 +10,40 @@ export default function BannerCarousel({ images }: { images: string[] }) {
     const prev = () => setIndex((i) => (i - 1 + images.length) % images.length);
 
     return (
-        <div className="banner-carousel">
-            <button className="banner-arrow left" onClick={prev}>
-                <img src="/left-arrow.svg" />
-            </button>
+        <div className="banner-wrapper">
+            <div className="banner-carousel">
+                <div
+                    className="banner-track"
+                    style={{ transform: `translateX(-${index * 100}%)` }}
+                >
+                    {images.map((img, i) => (
+                        <img key={i} src={img} className="banner-slide" />
+                    ))}
+                </div>
+            </div>
 
-            <img
-                src={images[index]}
-                className="banner-slide"
-                alt="banner"
-            />
+            <div className="banner-controls">
+                <div className="banner-dots">
+                    {images.map((_, i) => (
+                        <span
+                            key={i}
+                            className={`dot ${i === index ? "active" : ""}`}
+                            onClick={() => setIndex(i)}
+                        />
+                    ))}
+                </div>
 
-            <button className="banner-arrow right" onClick={next}>
-                <img src="/right-arrow.svg" />
-            </button>
+               
+                <div className="banner-bottom-arrows">
+                    <button className="bottom-arrow" onClick={prev}>
+                        <img src="/chevron-up (1).svg" alt="prev" />
+                    </button>                    
+                    
+                    <button className="bottom-arrow" onClick={next}>
+                        <img src="/chevron-up.svg" alt="next" />
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/Offers/Carousel/Carousel.tsx
+++ src/components/Offers/Carousel/Carousel.tsx
@@ -21,30 +21,46 @@ export default function Carousel({ items, visibleCount = 3 }: CarouselProps) {
 
     const visibleItems = getVisibleItems();
 
-    return (
-        <div className="carousel">
-            {/* Left arrow */}
-            <button className="carousel-arrow left" onClick={prev}>
-                <img src="/left-arrow.svg" alt="prev" />
-            </button>
+    const handleItemClick = (i: number) => {
+        if (i === 0) {
+            
+            prev();
+        } else if (i === 2) {
+           
+            next();
+        }
+       
+    };
 
-            {/* Cards wrapper */}
-            <div className="carousel-track">
-                {visibleItems.map((item, i) => (
-                    <div
-                        key={i}
-                        className={`carousel-item ${i === 1 ? "active" : "inactive"
-                            }`}
-                    >
-                        {item}
-                    </div>
-                ))}
+
+    return (
+        
+        <div className="carousel">
+
+            <div className="carousel">
+                <button className="carousel-arrow left" onClick={prev}>
+                    <img src="/chevron-up (1).svg" alt="prev" />
+                </button>
+
+                <div className="carousel-track">
+                    {visibleItems.map((item, i) => (
+                        <div
+                            key={i}
+                            className={`carousel-item ${i === 1 ? "active" : "inactive"}`}
+                            onClick={() => handleItemClick(i)}
+                        >
+                            {item}
+                        </div>
+                    ))}
+                </div>
+
+                <button className="carousel-arrow right" onClick={next}>
+                    <img src="/chevron-up.svg" alt="next" />
+                </button>
             </div>
 
-            {/* Right arrow */}
-            <button className="carousel-arrow right" onClick={next}>
-                <img src="/right-arrow.svg" alt="next" />
-            </button>
+
+          
         </div>
     );
 }
